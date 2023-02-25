d560f9cd75610f427cf99f2117e866eefd810a52
= <=>
2023-02-26 00:28:45 +0530

    Fixed the Intro Text

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,9 +7,9 @@ const Home = () => {
     <div name="Home" className='h-screen w-full bg-gradient-to-b from-black via-black to-gray-800'>
         <div className='max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row'>
             <div className="flex flex-col justify-center h-full">
-                <h2 className="text-4xl sm:text-7xl font-bold text-white">
-                    I'm a <span className=' underline text-pink-600'>Mohammad Faisal Sayed</span>
-                </h2>
+                <h3 className="text-4xl sm:text-7xl text-white">
+                    I'm <span className=' underline text-pink-600'>Mohammad Faisal Sayed</span>
+                </h3>
                 <p className=" text-[25px] text-fuchsia-500  py-10 max-w-md">
                 <Typewriter
                     options={{
